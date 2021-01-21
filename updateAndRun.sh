e45785088d71ee92a44a7f27554a1f2f4819b6d7
jtljac <jtljac2@gmail.com>
2021-01-21 18:37:59 +0000

    Updated updateFile

--- updateAndRun.sh
+++ updateAndRun.sh
@@ -5,10 +5,12 @@ oldVersion=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 git fetch --tags
 newVersion=$(git describe --tags "$(git rev-list --tags --max-count=1)")
 
-if [ "$oldVersion" != "$newVersion" ] || [ ! -f "MapExplorer.war" ]
+if [ "$oldVersion" != "$newVersion" ]
 then
         # Get Changes
         git checkout "$newVersion" -b master
+        git reset --hard origin/master
+        git clean -fd
         git pull
 
         chmod +x ./gradlew
