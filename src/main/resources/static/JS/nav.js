291d7edf3c31d46e6d0fed6638ca6a4a96777276
jtljac <jtljac2@gmail.com>
2021-01-22 02:23:06 +0000

    Started work on nav bar

--- src/main/resources/static/JS/nav.js
+++ src/main/resources/static/JS/nav.js
@@ -5,3 +5,67 @@ function openWithParams(path, params) {
     }
     window.open(newPath, "_self");
 }
+
+class Nav {
+    constructor(gallery, basePath, offset, numPerPage, search) {
+        this.gallery = gallery;
+        this.basePath = basePath;
+        this.offset = offset;
+        this.numPerPage = numPerPage;
+        this.search = search;
+        this.depth = 0;
+        this.gettingImages = false;
+        this.end = false;
+
+        this.getImages();
+        let scope = this;
+        window.onscroll = function (e) {
+            if (!scope.end && (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight * .9) {
+                scope.getImages();
+            }
+        }
+    }
+
+    createGalleryItem(map) {
+        let item = document.createElement("div");
+        item.className = "galleryitem";
+        let link = document.createElement("a");
+        link.href = "/image?id=" + map.id;
+        item.appendChild(link);
+        let wrap = document.createElement("div")
+        wrap.className = "imgwrap";
+        link.appendChild(wrap)
+        let image = document.createElement("img");
+        image.src = this.basePath + map.filePath;
+        if (map.squareWidth && map.squareWidth) image.alt = map.squareWidth + "x" + map.squareHeight;
+        wrap.appendChild(image);
+
+        return item;
+    }
+
+    callback(response){
+        let maps = JSON.parse(response.responseText);
+        for (let map of maps) {
+            this.gallery.appendChild(this.createGalleryItem(map));
+        }
+        this.gettingImages = false;
+    }
+
+    getImages() {
+        if (!this.gettingImages) {
+            let scope = this;
+            ajaxSimpleGet("/getImages?search=" + this.search + "&offset=" + (this.offset + this.numPerPage * this.depth) + "&numPerPage=" + this.numPerPage, function (response) {
+                if (response.readyState === 4) {
+                    if (response.status === 200) scope.callback(response);
+                    else if (response.status === 204) {
+                        console.info("Run out of images to load")
+                        scope.end = true;
+                    }
+                }
+            });
+            this.depth += 1;
+            this.gettingImages = true;
+        }
+    }
+}
+
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -3,17 +3,19 @@
     <head>
         <link href="CSS/stylesheet.css" rel="stylesheet">
         <script src="JS/nav.js"></script>
-        <script src="JS/search.js"></script>
         <script src="JS/AJAX.js"></script>
         <title>Battle map Finder</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+              rel="stylesheet">
     </head>
     <body>
         <nav>
-
+            <div style = "background-color: white; height: 100%; width: 30px;"></div>
+            <input id="searchText" value="${search}"  type="text" placeholder="Search for some tags"/><button id="search" type="submit"><span class="material-icons md-light">search</span></button>
         </nav>
         <main>
-            <input id="searchText" value="${search}"  type="text" placeholder="Search for some tags"/><button id="search" type="submit">Search</button>
-            <div id="gallery" class="gallery" onload="">
+            <div id="gallery" class="gallery">
             </div>
         </main>
     <script>
@@ -28,7 +30,7 @@
             }
         })
 
-        const search = new Search(document.getElementById("gallery"), "${basePath}", ${offset}, ${numPerPage}, "${search}");
+        const search = new Nav(document.getElementById("gallery"), "${basePath}", ${offset}, ${numPerPage}, "${search}");
     </script>
     </body>
 </html>
