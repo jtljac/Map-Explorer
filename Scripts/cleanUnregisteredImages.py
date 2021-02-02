351d5df16a6c6af23ba1254c0bbfa7605d4275f4
jtljac <jtljac2@gmail.com>
2021-02-02 04:05:24 +0000

    Fin

--- Scripts/cleanUnregisteredImages.py
+++ Scripts/cleanUnregisteredImages.py
@@ -1,12 +1,14 @@
 import cymysql, os
 
-
 def parseDir(directory):
+    global counter
     for file in os.scandir(directory):
         if file.is_file():
             path = file.path.replace("\\", "/")[len(startingDirectory):]
             if path not in result:
                 os.remove(file.path)
+                print("Removed " + path)
+                counter += 1
 
         elif file.is_dir():
             parseDir(file.path)
@@ -20,7 +22,11 @@ cursor.execute(sql)
 result = [item[0] for item in cursor.fetchall()]
 conn.close()
 
+counter = 0
 startingDirectory = input("Enter Starting Directory: ").replace("\\", "/")
+
 if startingDirectory[-1] != "/":
     startingDirectory += "/"
 parseDir(startingDirectory)
+
+print("Cleaned " + str(counter) + " unregistered files")
