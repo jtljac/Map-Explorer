8c2d9fe5d29c04bfd64d38303e5bfba6fb7a05f3
Jacob Lucas <jtljac2@gmail.com>
2021-04-10 04:44:28 +0100

    Fixed incorrect pagable import

--- src/main/java/com/datdev/repo/CollectionRepo.java
+++ src/main/java/com/datdev/repo/CollectionRepo.java
@@ -1,10 +1,10 @@
 package com.datdev.repo;
 
 import com.datdev.model.MapCollection;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface CollectionRepo extends CrudRepository<MapCollection, Integer> {
