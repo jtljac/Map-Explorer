77fc869c8d2610b25393371dd150deb9f50544e5
Jacob Lucas <jtljac2@gmail.com>
2021-04-10 04:38:31 +0100

    Fixed Collectionrepo preventing start

--- src/main/java/com/datdev/repo/CollectionRepo.java
+++ src/main/java/com/datdev/repo/CollectionRepo.java
@@ -10,5 +10,6 @@ import java.util.List;
 public interface CollectionRepo extends CrudRepository<MapCollection, Integer> {
     boolean existsByName(String name);
 
+    @Query(value = "SELECT m FROM MapCollection m")
     List<MapCollection> findAll(Pageable pageable);
 }
